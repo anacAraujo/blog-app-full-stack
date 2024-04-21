c54bd5a24750f21339edc47045d2d157b8ee8996
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-04-21 23:06:50 +0100

    add logs to auth

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -6,8 +6,13 @@ export const register = (req, res) => {
   //CHECK EXISTING USER
   const q = "SELECT * FROM users WHERE email = ? OR username = ?";
 
+  console.log("Register with: ", req.body);
+
   db.query(q, [req.body.email, req.body.username], (err, data) => {
-    if (err) return res.status(500).json(err);
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: "Internal server error!" });
+    }
     if (data.length) return res.status(409).json("User already exists!");
 
     //Hash the password and create a user
