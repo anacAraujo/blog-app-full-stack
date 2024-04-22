98955f9cd6815d2c25fc2722b0a1b4bef39b7734
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-04-22 20:11:05 +0100

    use mysql connection pool

--- api/db.js
+++ api/db.js
@@ -1,12 +1,19 @@
 import mysql from "mysql2/promise";
 
-// Create the connection to database
-export const db = await mysql.createConnection({
+// Create the connection pool. The pool-specific settings are the defaults
+export const db = mysql.createPool({
   // TODO create .env file and add the following variables
   host: "localhost",
   user: "root",
   password: "root",
   database: "blog",
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000 (1min)
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
 console.log("Connected to database!");
